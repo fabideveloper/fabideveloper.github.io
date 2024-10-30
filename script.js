201b3d9951ba930a65f7042132db300b84fff645
Fabian <96831684+fabideveloper@users.noreply.github.com>
2024-10-30 18:04:52 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+// Placeholder for JavaScript interactivity
+document.addEventListener('DOMContentLoaded', () => {
+    console.log("JavaScript is ready to go!");
+    // Additional interactive scripts can be added here
+});
