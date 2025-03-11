9d24d8453d17b621ebf095f1c29c644fa46df9ab
Fabian <96831684+fabideveloper@users.noreply.github.com>
2025-03-11 16:15:58 +0200

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-// Placeholder for JavaScript interactivity
-document.addEventListener('DOMContentLoaded', () => {
-    console.log("JavaScript is ready to go!");
-    // Additional interactive scripts can be added here
-});
